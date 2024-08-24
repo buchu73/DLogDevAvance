6dbc420dea5e4aec8b343dbc4de1f865b891569c
Jean-Charles Durand <jc.buchu@gmail.com>
2024-08-24 12:11:01 +0200

    Add C# code base

---
+++ ProgAsynchrone-ChatApp/VersionDeBase/C#/ChatApp/Client/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Program
+    {
+        private const string ServerAddress = "localhost";
+        private const int ServerPort = 12345;
+
+        static async Task Main(string[] args)
+        {
+            using (var client = new TcpClient(ServerAddress, ServerPort))
+            {
+                // Start the task for receiving messages
+                var receiveTask = Task.Run(() => ReceiveMessagesAsync(client));
+
+                // Start the task for sending messages
+                var sendTask = Task.Run(() => SendMessagesAsync(client));
+
+                await Task.WhenAll(receiveTask, sendTask);
+            }
+        }
+
+        private static async Task ReceiveMessagesAsync(TcpClient client)
+        {
+            using (var stream = client.GetStream())
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    try
+                    {
+                        string message;
+                        while ((message = await reader.ReadLineAsync()) != null)
+                        {
+                            Console.WriteLine("\r" + message);
+                            Console.Write("You: ");
+                        }
+                    }
+                    catch (IOException)
+                    {
+                        // Handle disconnection
+                    }
+                }
+            }
+        }
+
+        private static async Task SendMessagesAsync(TcpClient client)
+        {
+            using (var stream = client.GetStream())
+            {
+                using (var writer = new StreamWriter(stream) { AutoFlush = true })
+                {
+                    try
+                    {
+                        string userInput;
+                        while ((userInput = Console.ReadLine()) != null)
+                        {
+                            await writer.WriteLineAsync(userInput);
+                            Console.Write("You: ");
+                        }
+                    }
+                    catch (IOException)
+                    {
+                        // Handle disconnection
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ProgAsynchrone-ChatApp/VersionDeBase/C#/ChatApp/Server/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class Program
+    {
+        private const int Port = 12345;
+        static void Main(string[] args)
+        {
+            var listener = new TcpListener(IPAddress.Any, Port);
+            listener.Start();
+            Console.WriteLine("Chat server started on port " + Port);
+
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+                Task.Run(() => HandleClientMessagesAsync(client));
+            }
+        }
+
+        private static async Task HandleClientMessagesAsync(TcpClient client)
+        {
+            using (var stream = client.GetStream())
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+
+                    // Read user name
+                    await writer.WriteLineAsync("Enter your name: ");
+                    string userName = await reader.ReadLineAsync();
+                    Console.WriteLine(userName + " has joined the chat.");
+
+                    try
+                    {
+                        string message;
+                        while ((message = await reader.ReadLineAsync()) != null)
+                        {
+                            Console.WriteLine(userName + ": " + message);
+                        }
+                    }
+                    catch (IOException)
+                    {
+                        // Handle disconnection
+                    }
+                    finally
+                    {
+                        Console.WriteLine($"{userName} has left the chat.");
+                    }
+                }
+            };
+        }
+    }
+}
